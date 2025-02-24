1d0efae297371f2892c2b481dc0e23e3bdfa9974
vini-jose <assisjose481@gmail.com>
2025-02-23 23:15:37 -0300

    botando o css e o script dentro de uma pasta

---
+++ script/script.js
@@ -0,0 +1,57 @@
+function openPopup(category) {
+    const popup = document.getElementById('popup');
+    const popupTitle = document.getElementById('popupTitle');
+    const popupList = document.getElementById('popupList');
+
+    
+    switch (category) {
+        case 'Cursos':
+            popupTitle.textContent = 'Documentos para Cursos';
+            popupList.innerHTML = `
+                <li>Comprovante de matrícula</li>
+                <li>Histórico escolar</li>
+                <li>Identidade</li>
+            `;
+            break;
+        case 'Lazer':
+            popupTitle.textContent = 'Documentos para Lazer';
+            popupList.innerHTML = `
+                <li>Comprovante de residência</li>
+                <li>CPF</li>
+                <li>Identidade</li>
+            `;
+            break;
+        case 'Preferencial':
+            popupTitle.textContent = 'Documentos para Preferencial';
+            popupList.innerHTML = `
+                <li>Comprovante de residência</li>
+                <li>Laudo médico</li>
+                <li>Identidade</li>
+            `;
+            break;
+        case 'Saúde':
+            popupTitle.textContent = 'Documentos para Saúde';
+            popupList.innerHTML = `
+                <li>Comprovante de residência</li>
+                <li>Carteirinha do SUS</li>
+                <li>Identidade</li>
+            `;
+            break;
+        default:
+            popupTitle.textContent = 'Documentos necessários';
+            popupList.innerHTML = `
+                <li>Comprovante de residência</li>
+                <li>CPF</li>
+                <li>Identidade</li>
+            `;
+    }
+
+    
+    popup.style.display = 'flex';
+}
+
+
+function closePopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'none';
+}
